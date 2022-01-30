63506ff3c149fc0cb8a69c84ebd7ab6689b9c156
pr0head <pr0head@gmail.com>
2022-01-30 16:06:12 +0300

    Use unistack micro server

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.5.2
 	github.com/micro/go-micro v1.18.0
+	go.unistack.org/micro-network-transport-http/v3 v3.8.0
+	go.unistack.org/micro-register-etcd/v3 v3.8.1
 	go.unistack.org/micro/v3 v3.8.20
 	google.golang.org/protobuf v1.27.1
 )
--- internal/app.go
+++ internal/app.go
@@ -2,8 +2,12 @@ package internal
 
 import (
 	"context"
-	"github.com/micro/go-micro"
 	micro_old_server "github.com/pr0head/micro-old-server/pkg"
+	microHttp "go.unistack.org/micro-network-transport-http/v3"
+	etcd "go.unistack.org/micro-register-etcd/v3"
+	"go.unistack.org/micro/v3"
+	"go.unistack.org/micro/v3/register"
+	"go.unistack.org/micro/v3/server"
 )
 
 type Application struct {
@@ -21,14 +25,27 @@ func NewApplication() *Application {
 func (app *Application) Run() {
 	app.svc = &Service{}
 
+	tr := microHttp.NewTransport()
+	r := etcd.NewRegister(
+		register.Addrs("127.0.0.1:2379"),
+	)
+
+	serverOptions := []server.Option{
+		server.Register(r),
+		server.Name(micro_old_server.ServiceName),
+		server.Transport(tr),
+	}
+	microServer := server.NewServer(serverOptions...)
+
 	options := []micro.Option{
 		micro.Name(micro_old_server.ServiceName),
+		micro.Server(microServer),
 	}
 
 	app.service = micro.NewService(options...)
 	app.service.Init()
 
-	if err := micro_old_server.RegisterMicroOldServerHandler(app.service.Server(), app.svc); err != nil {
+	if err := micro_old_server.RegisterMicroOldServerServer(app.service.Server(), app.svc); err != nil {
 		panic("Can`t register service in micro")
 	}
 
